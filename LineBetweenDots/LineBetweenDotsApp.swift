a42e31bb756c0ba4e8c94fbc3b2355f831cbc1ec
id2k1149 <id2k1149@gmail.com>
2023-05-09 20:09:03 -0400

    Initial Commit

---
+++ LineBetweenDots/LineBetweenDotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineBetweenDotsApp.swift
+//  LineBetweenDots
+//
+//  Created by Max Franz Immelmann on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LineBetweenDotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
